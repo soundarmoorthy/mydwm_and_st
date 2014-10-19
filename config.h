f76bf44bee785edfeab0000983786f9cb9b1a7fa
Soundararajan Dhakshinamoorthy <er.soundararajan@gmail.com>
2014-10-19 17:38:19 +0530

    Add st to the repository. Update font settings and shell customization settings to dwm and st

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-Bitstream Vera Sans-medium-r-*-*-999999999-*-*-*-*-*-*-*";
+static const char font[]            = "-*-Ubuntu Mono-12-r-*-*-999999999-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#111111";
 static const char normfgcolor[]     = "#bbbbbb";
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
